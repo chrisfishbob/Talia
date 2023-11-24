b356d4b8558d40ff95feb92d8e04a8ccfa437fbc
Chris Hsu <chrisfishbob@gmail.com>
2023-11-24 14:31:18 -0800

    Added Game struct to encapsulate game manger functions

--- src/game_manager.rs
+++ src/game_manager.rs
@@ -15,45 +15,34 @@ enum GameState {
     Stalemate,
 }
 
-pub fn start_new_game(
-    fen: Option<&str>,
+pub struct Game {
     player_color: Option<Color>,
-    engine_search_depth: u32,
-) -> Result<(), BoardError> {
-    let mut board: Board = match fen {
-        None => BoardBuilder::from_starting_position().try_into()?,
-        Some(fen) => BoardBuilder::try_from_fen(fen)?,
-    };
+    board: Board,
+    engine_search_depth: u32
+}
 
-    loop {
-        let mut move_generator = MoveGenerator::new(board.clone());
-        match check_game_state(&mut move_generator) {
-            GameState::Active => {}
-            GameState::Checkmate => {
-                println!("Checkmate!");
-                return Ok(());
-            }
-            GameState::Stalemate => {
-                println!("Stalemate!");
-                return Ok(());
-            }
-        }
+impl Game {
+    pub fn try_from_fen(
+        fen: Option<&str>,
+        player_color: Option<Color>,
+        engine_search_depth: u32,
+    ) -> Result<Self, BoardError> {
+        let board: Board = match fen {
+            None => BoardBuilder::from_starting_position().try_into()?,
+            Some(fen) => BoardBuilder::try_from_fen(fen)?,
+        };
 
-        if player_color.is_some_and(|color| color == board.to_move) {
-            println!("{}", board);
-            let input = get_uci_move_input();
-            match Move::try_from_algebraic_notation(&input, &mut move_generator) {
-                Ok(mv) => board.move_piece(&mv),
-                Err(error) => println!("{}", error),
-            }
-        } else {
-            // Talia plays
-            // Only print the board while Talia is thinking if there is no human player
-            if player_color.is_none() {
-                println!("{}", board);
-            }
+        Ok(Self {
+            player_color,
+            board,
+            engine_search_depth,
+        })
+    }
 
-            match check_game_state(&mut move_generator) {
+    pub fn start_game(&mut self) -> Result<(), BoardError> {
+        loop {
+            let mut move_generator = MoveGenerator::new(self.board.clone());
+            match self.check_game_state(&mut move_generator) {
                 GameState::Active => {}
                 GameState::Checkmate => {
                     println!("Checkmate!");
@@ -65,54 +54,81 @@ pub fn start_new_game(
                 }
             }
 
-            println!("Talia is thinking ...");
-            let start_time = std::time::Instant::now();
-            let (best_move, mut best_eval) = find_best_move(
-                &mut move_generator.generate_moves(),
-                &mut move_generator,
-                engine_search_depth,
-            );
-            let end_time = std::time::Instant::now();
-            let elapsed_time = end_time.duration_since(start_time).as_millis();
-            println!(
-                "Talia thought for {} milliseconds and evaluted {} positions at depth {}",
-                elapsed_time,
-                COUNTER.load(Ordering::Relaxed),
-                engine_search_depth
-            );
-
-            println!("Best move: {:?}", best_move);
-
-            // Display the eval without perspective.
-            // Positive eval: white has advantage, negative eval: black has advantage
-            if move_generator.board.to_move == Color::Black {
-                best_eval *= -1
-            }
-            board.move_piece(&best_move);
-            println!("Eval: {best_eval}")
-        }
-    }
-}
-
-fn check_game_state(move_generator: &mut MoveGenerator) -> GameState {
-    let moves = move_generator.generate_moves();
-    match moves.is_empty() {
-        true => {
-            if move_generator.is_in_check(move_generator.board.to_move) {
-                GameState::Checkmate
+            if self.player_color.is_some_and(|color| color == self.board.to_move) {
+                println!("{}", self.board);
+                let input = self.get_uci_move_input();
+                match Move::try_from_algebraic_notation(&input, &mut move_generator) {
+                    Ok(mv) => self.board.move_piece(&mv),
+                    Err(error) => println!("{}", error),
+                }
             } else {
-                GameState::Stalemate
+                // Talia plays
+                // Only print the board while Talia is thinking if there is no human player
+                if self.player_color.is_none() {
+                    println!("{}", self.board);
+                }
+
+                match self.check_game_state(&mut move_generator) {
+                    GameState::Active => {}
+                    GameState::Checkmate => {
+                        println!("Checkmate!");
+                        return Ok(());
+                    }
+                    GameState::Stalemate => {
+                        println!("Stalemate!");
+                        return Ok(());
+                    }
+                }
+
+                println!("Talia is thinking ...");
+                let start_time = std::time::Instant::now();
+                let (best_move, mut best_eval) = find_best_move(
+                    &mut move_generator.generate_moves(),
+                    &mut move_generator,
+                    self.engine_search_depth,
+                );
+                let end_time = std::time::Instant::now();
+                let elapsed_time = end_time.duration_since(start_time).as_millis();
+                println!(
+                    "Talia thought for {} milliseconds and evaluted {} positions at depth {}",
+                    elapsed_time,
+                    COUNTER.load(Ordering::Relaxed),
+                    self.engine_search_depth
+                );
+
+                println!("Best move: {:?}", best_move);
+
+                // Display the eval without perspective.
+                // Positive eval: white has advantage, negative eval: black has advantage
+                if move_generator.board.to_move == Color::Black {
+                    best_eval *= -1
+                }
+                self.board.move_piece(&best_move);
+                println!("Eval: {best_eval}")
             }
         }
-        false => GameState::Active,
     }
-}
 
-fn get_uci_move_input() -> String {
-    let mut input = String::new();
-    std::io::stdin()
-        .read_line(&mut input)
-        .expect("failed to read line");
+    fn check_game_state(&self, move_generator: &mut MoveGenerator) -> GameState {
+        let moves = move_generator.generate_moves();
+        match moves.is_empty() {
+            true => {
+                if move_generator.is_in_check(move_generator.board.to_move) {
+                    GameState::Checkmate
+                } else {
+                    GameState::Stalemate
+                }
+            }
+            false => GameState::Active,
+        }
+    }
 
-    input.trim().to_owned()
+    fn get_uci_move_input(&self) -> String {
+        let mut input = String::new();
+        std::io::stdin()
+            .read_line(&mut input)
+            .expect("failed to read line");
+
+        input.trim().to_owned()
+    }
 }
