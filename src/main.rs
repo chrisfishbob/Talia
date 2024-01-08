13233fa2825335f65281d17864bea11e6eb8e039
Chris Hsu <chrisfishbob@gmail.com>
2024-01-07 19:08:00 -0800

    command line mode can now be used instead of UCI with --cli flag

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use clap::Parser;
 
 pub mod board;
 pub mod board_builder;
@@ -11,18 +12,29 @@ pub mod piece_square_table;
 pub mod search;
 pub mod square;
 use crate::bot::Bot;
-// use crate::game_manager::Game;
-// use crate::piece::Color;
+use crate::game_manager::Game;
+use crate::piece::Color;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct Args {
+    #[arg(long, default_value_t = false)]
+    cli: bool,
+}
 
 fn main() -> Result<()> {
     println!("Talia Chess Engine: v1.1.1");
+    let args = Args::parse();
 
-    // let search_depth = 6;
-    // let fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-    // let mut game = Game::try_from_fen(fen, Some(Color::White), search_depth)?;
-    // game.start_game()?;
-    let mut bot = Bot::new();
-    bot.start_uci()?;
+    if args.cli {
+        let search_depth = 6;
+        let fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+        let mut game = Game::try_from_fen(fen, Some(Color::White), search_depth)?;
+        game.start_game()?;
+    } else {
+        let mut bot = Bot::new();
+        bot.start_uci()?;
+    }
 
     Ok(())
 }
